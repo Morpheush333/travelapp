45a2a91b00dc7d10da6c166217ca3c2cfc49a14a
Mateusz Medon <mateuszmedon1@gmail.com>
2019-10-05 11:44:48 +0100

    git init

---
+++ src/test/java/com/mateuszmedon/travelapp/TravelappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mateuszmedon.travelapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
