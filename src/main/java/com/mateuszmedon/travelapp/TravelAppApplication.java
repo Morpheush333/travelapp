78fb1aa1719fa909a9743325b3cdc0d16b8b1cf8
Mateusz Medon <mateuszmedon1@gmail.com>
2019-10-05 12:29:18 +0100

    mysql conector configuration

--- src/main/java/com/mateuszmedon/travelapp/TravelAppApplication.java
+++ src/main/java/com/mateuszmedon/travelapp/TravelAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelappApplication {
+public class TravelAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TravelappApplication.class, args);
+        SpringApplication.run(TravelAppApplication.class, args);
     }
 
 }
